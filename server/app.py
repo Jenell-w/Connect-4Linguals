e21f6395adabc0534ac022e6ac296dbb20c41a38
peter kurjanowicz <pkurjanowicz5@gmail.com>
2020-06-03 21:08:13 -0600

    finished making endpoints and handling user session

--- server/app.py
+++ server/app.py
@@ -8,6 +8,7 @@ app = Flask(__name__,
             static_folder="./dist/static",
             template_folder="./dist"
             )
+app.secret_key = "@H238sd&ew9@#lso@Apso"
 app.register_blueprint(topics_api)
 app.register_blueprint(auth_api)
 
--- server/authAPI.py
+++ server/authAPI.py
@@ -1,23 +1,80 @@
 from flask import Blueprint, jsonify, request, session
 import requests
 import json
-from harperdb_instance import response, url, headers
-
+from harperdb_instance import url, headers
+import bcrypt
 
 auth_api = Blueprint('auth_api', __name__)
 
-@auth_api.route('/login', methods=['GET', 'POST'])
-def login():
-  payload = '''{
-	"operation":"insert",
+def find_if_user_name_exists(username):
+  payload = {
+	"operation":"search_by_value",
 	"schema":"ConnectLinguals",
 	"table":"users",
-	"records": [
-      {
-        "username" : "Pete",
-        "pass": "XXXXXXX"
-      }
-    ]
-}'''
-  response = requests.request("POST", url, headers=headers, data=payload)
-  return response.text.encode('utf8')
+	"search_attribute":"username",
+	"search_value":"{}".format(username),
+	"get_attributes": ["*"]
+  }
+  try: 
+    response = requests.request("POST", url, headers=headers, data=json.dumps(payload)).json()[0]
+  except IndexError:
+    return "False"
+  return "True"
+
+@auth_api.route('/login', methods=['GET', 'POST'])
+def login():
+  session['user'] = ''
+  password = request.json["password"].encode('UTF-8')
+  username = request.json["username"]
+  payload = {
+	"operation":"search_by_value",
+	"schema":"ConnectLinguals",
+	"table":"users",
+	"search_attribute":"username",
+	"search_value":"{}".format(username),
+	"get_attributes": ["*"]
+  }
+  hashed = requests.request("POST", url, headers=headers, data=json.dumps(payload)).json()[0]['pass']
+  if bcrypt.checkpw(password, hashed.encode('UTF-8')):
+    session['user'] = username
+    usernamesession = session['user']
+    return jsonify(session=usernamesession)
+  else:
+    return jsonify(success=False)
+
+
+@auth_api.route('/register', methods=['GET', 'POST'])
+def register():
+  session['user'] = ''
+  username = request.json["username"]
+  if find_if_user_name_exists(username) == "True":
+    return "Name Already Exists"
+  password = request.json["password"].encode('UTF-8')
+  hashed = bcrypt.hashpw(password, bcrypt.gensalt())
+  decoded_hashed = hashed.decode('utf8')
+  payload = {
+    "operation":"insert",
+    "schema":"ConnectLinguals",
+    "table":"users",
+    "records": [
+        {
+          "username" : username,
+          "pass": "{}".format(decoded_hashed)
+        }
+      ]
+  }
+  response = requests.request("POST", url, headers=headers, data=json.dumps(payload))
+  session['user'] = username
+  usernamesession = session['user']
+  return jsonify(session=usernamesession)
+
+
+@auth_api.route('/checksession', methods=["GET"])
+def check_session():
+        if 'user' in session:
+                return jsonify(
+                        session = True,
+                        user = session['user']
+                )
+        else:
+                return jsonify(session = False)
