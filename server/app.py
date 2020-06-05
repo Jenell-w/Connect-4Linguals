97fa168b75dc213e40ca8bb35517578095f8b88c
Jenell-w <jenellweitz@gmail.com>
2020-06-04 21:38:33 -0600

    fixed app.py

--- server/app.py
+++ server/app.py
@@ -1,16 +1,20 @@
 from flask import Flask, render_template, jsonify
 import requests
 from topicsAPI import topics_api
+from authAPI import auth_api
 
 app = Flask(__name__,
             static_folder="./dist/static",
             template_folder="./dist"
             )
+
+app.secret_key = "@H238sd&ew9@#lso@Apso"
 app.register_blueprint(topics_api)
+app.register_blueprint(auth_api)
 
 
 @app.route('/', defaults={'path': ''})
-@app.route("/<path:path>")
+@app.route('/<path:path>')
 def catch_all(path):
     return(render_template('index.html'))
 
